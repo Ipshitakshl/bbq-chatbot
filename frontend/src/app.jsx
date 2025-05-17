ccf13164712bf8eede7457f514c1226123e3132e
Ipshitakshl <ipshitakshl0111@gmail.com>
2025-05-18 02:24:51 +0530

    Create app.jsx

---
+++ frontend/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ChatInterface from './components/ChatInterface.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <h1>BBQ Nation Chatbot</h1>
+      <ChatInterface />
+    </div>
+  )
+}
